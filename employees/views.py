d2689c631d0d359e7779f809fd063130b6bf7ef7
Phuvanenthran-P <phuvan0205@gmail.com>
2025-12-20 19:59:58 +0530

    Add employee list and create views with templates

--- employees/views.py
+++ employees/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Employee
 
-# Create your views here.
+def employee_list(request):
+    employees = Employee.objects.all()
+    return render(request, 'employees/list.html', {'employees': employees})
+
+
+def employee_create(request):
+    if request.method == "POST":
+        Employee.objects.create(
+            first_name=request.POST['first_name'],
+            last_name=request.POST['last_name'],
+            email=request.POST['email'],
+            department=request.POST['department'],
+            salary=request.POST['salary'],
+        )
+        return redirect('employee_list')
+
+    return render(request, 'employees/create.html')
