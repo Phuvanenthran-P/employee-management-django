e2bc2462790979ddd83cbf270b25e833e86ef86a
Phuvanenthran-P <phuvan0205@gmail.com>
2025-12-20 21:31:26 +0530

    Configure Django admin and enforce permissions on employee views

--- employees/admin.py
+++ employees/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Employee
 
-admin.site.register(Employee)
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'department', 'salary')
+    search_fields = ('first_name', 'last_name', 'email')
+    list_filter = ('department',)
--- employees/views.py
+++ employees/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render, redirect
 from .models import Employee
 from .forms import EmployeeForm
 from django.shortcuts import get_object_or_404
+from django.contrib.auth.decorators import login_required, permission_required
+
 
 def employee_list(request):
     employees = Employee.objects.all()
     return render(request, 'employees/list.html', {'employees': employees})
 
+@login_required
+@permission_required('employees.add_employee', raise_exception=True)
 def employee_create(request):
     if request.method == "POST":
         form = EmployeeForm(request.POST)
@@ -18,6 +22,8 @@ def employee_create(request):
 
     return render(request, 'employees/create.html', {'form': form})
 
+@login_required
+@permission_required('employees.change_employee', raise_exception=True)
 def employee_update(request, pk):
     employee = get_object_or_404(Employee, pk=pk)
 
@@ -34,6 +40,8 @@ def employee_update(request, pk):
         'employee': employee
     })
 
+@login_required
+@permission_required('employees.delete_employee', raise_exception=True)
 def employee_delete(request, pk):
     employee = get_object_or_404(Employee, pk=pk)
 
