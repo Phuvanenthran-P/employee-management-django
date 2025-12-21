aebf4acb0d85110fbbc996bdd4011acf1253d98d
Phuvanenthran-P <phuvan0205@gmail.com>
2025-12-21 19:14:48 +0530

    Added re-direction to login page

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('employees/', include('employees.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('employees.urls')),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+
 ]
